18f2aeaf5327304f7ff7372ebb2f4d78d6e12df2
GokaiInfinity <patriciofilbert@gmail.com>
2023-12-06 15:30:02 +0700

    Intro 1

--- app/src/main/java/com/example/lyrifyapp/ui/screen/Intro/Intro1.kt
+++ app/src/main/java/com/example/lyrifyapp/ui/screen/Intro/Intro1.kt
@@ -1,2 +1,78 @@
 package com.example.lyrifyapp.ui.screen.Intro
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lyrifyapp.R
+import com.example.lyrifyapp.ui.theme.Background
+import com.example.lyrifyapp.ui.theme.montserrat
+
+@Composable
+private fun loadingview(){
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Background)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            // Replace R.drawable.your_logo with your actual logo resource ID
+            val logo = painterResource(id = R.drawable.logo)
+            Image(
+                painter = logo,
+                contentDescription = "Logo",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .size(200.dp) // Adjust size as needed
+            )
+        }
+        Text(
+            text = "Decode, Learn, Listen",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            fontFamily = montserrat,
+            fontWeight = FontWeight(400),
+            fontStyle = FontStyle.Italic,
+            fontSize = 16.sp,
+            color = Color(0xFFFFFFFF),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun loadingpreview(){
+    loadingview()
+}
