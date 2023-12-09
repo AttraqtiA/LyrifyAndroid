aaf8ed48f69c9eedd12e684d1ee5ca4800b1ec3a
GokaiInfinity <patriciofilbert@gmail.com>
2023-12-09 20:20:13 +0700

    Intro & Login

--- app/src/main/java/com/example/lyrifyapp/ui/screen/Intro/Intro3.kt
+++ app/src/main/java/com/example/lyrifyapp/ui/screen/Intro/Intro3.kt
@@ -1,2 +1,140 @@
 package com.example.lyrifyapp.ui.screen.Intro
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lyrifyapp.R
+import com.example.lyrifyapp.ui.theme.Background
+import com.example.lyrifyapp.ui.theme.Purple2
+import com.example.lyrifyapp.ui.theme.montserrat
+
+@Composable
+private fun loading2view(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Background)
+    ) {
+        val text = buildAnnotatedString {
+            append("Elevate your ")
+            withStyle(
+                style = SpanStyle(
+                    color = Color(0xFFFFA500), // Orange color
+                    fontWeight = FontWeight(800),
+                    letterSpacing = 1.04.sp,
+                )
+            ) {
+                append("Vocabulary ")
+            }
+            append("through melody.")
+
+        }
+
+        Text(
+            text = text,
+            style = TextStyle(
+                fontSize = 32.sp,
+                lineHeight = 31.2.sp,
+                fontFamily = montserrat,
+                fontWeight = FontWeight(800),
+                color = Color(0xFFFFFFFF),
+
+                letterSpacing = 1.04.sp,
+            ),
+            modifier = Modifier.padding(top = 32.dp, end = 32.dp, start= 32.dp)
+        )
+        Text(
+            text = "Dive into the lyrics, unravel the words, and elevate your vocabulary.",
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontFamily = montserrat,
+                fontWeight = FontWeight(400),
+                color = Color(0xFFFFFFFF),
+
+                ),
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
+        Box(
+            contentAlignment = Alignment.BottomCenter,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.asset_2),
+                contentDescription = "Listener",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+//                    .fillMaxWidth()
+                    .size(600.dp)
+            )
+            Column (
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(bottom = 24.dp)
+                    .height(64.dp)
+            ){
+
+                Image(
+                    painter = painterResource(id = R.drawable.pagination_2),
+                    contentDescription = "image description",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .width(40.dp)
+                        .height(10.dp)
+
+
+                )
+
+                Button(onClick = { /**/},
+                    colors = ButtonDefaults.buttonColors(containerColor = Purple2)) {
+                    Text(
+                        text = "Next",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontFamily = montserrat,
+                            fontWeight = FontWeight(800),
+                            color = Color(0xFFFFFFFF),
+
+                            textAlign = TextAlign.Center,
+                        )
+                    )
+                }
+
+            }
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun loading2preview(){
+    loading2view()
+}
--- app/src/main/java/com/example/lyrifyapp/ui/screen/Login/LoginView.kt
+++ app/src/main/java/com/example/lyrifyapp/ui/screen/Login/LoginView.kt
@@ -1,2 +1,219 @@
-package com.example.lyrifyapp.ui.screen.Login
+package com.example.lyrifyapp.ui.screen.Intro
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lyrifyapp.R
+import com.example.lyrifyapp.ui.theme.Background
+import com.example.lyrifyapp.ui.theme.Orange
+import com.example.lyrifyapp.ui.theme.Purple80
+import com.example.lyrifyapp.ui.theme.montserrat
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun loginview(){
+    var email by remember { mutableStateOf("") }
+    var pass by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Lyrify",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.width(96.dp)
+        )
+        Text(
+            text = "Welcome Back",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontFamily = montserrat,
+                fontWeight = FontWeight(700),
+                color = Color(0xFFFFFFFF),
+
+                textAlign = TextAlign.Center,
+            )
+        )
+        Text(
+            text = "Enter your credentials and continue your journey with us.",
+            style = TextStyle(
+                fontSize = 12.sp,
+                fontFamily = montserrat,
+                fontWeight = FontWeight(400),
+                color = Color(0xFFB6B6B6),
+
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier
+                .width(195.dp)
+                .height(30.dp)
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+            email = it
+            },
+            modifier = Modifier.padding(top = 16.dp)
+                .width(320.dp)
+                .height(64.dp),
+                shape = RoundedCornerShape(8.dp),
+
+            label = {
+                Text(
+                    text = "Email Address",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = montserrat,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFFFFFFFF),
+
+                        textAlign = TextAlign.Center,
+                    ),
+                    modifier = Modifier.padding(top=2.dp),
+
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Orange,
+                unfocusedBorderColor = Orange
+            )
+        )
+        OutlinedTextField(
+            value = pass,
+            onValueChange = {
+                pass = it
+            },
+            modifier = Modifier.padding(vertical = 16.dp)
+                .width(320.dp)
+                .height(64.dp),
+            shape = RoundedCornerShape(8.dp),
+
+            label = {
+                Text(
+                    text = "Password",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = montserrat,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFFFFFFFF),
+
+                        textAlign = TextAlign.Center,
+                    ),
+                    modifier = Modifier.padding(top=2.dp),
+
+                    )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Orange,
+                unfocusedBorderColor = Orange
+            ),
+            trailingIcon = {
+                IconButton(
+                    onClick = { /**/ }
+                ) {
+                    Icon(
+                          imageVector = Icons.Default.Place,
+                            contentDescription = "Visible"
+                    )
+
+                }
+            },
+        )
+
+        Button(onClick = {/**/},
+            colors = ButtonDefaults.buttonColors(containerColor = Orange),
+            shape = RoundedCornerShape(7.dp),
+            modifier = Modifier
+                .width(320.dp)
+                .height(50.dp)
+
+        ) {
+            Text(
+                text = "Login",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = montserrat,
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFFFFFFFF),
+
+                    textAlign = TextAlign.Center,
+                ),
+            )
+
+
+        }
+        val text = buildAnnotatedString {
+            append("Didn't have an account? ")
+            withStyle(
+                style = SpanStyle(
+                    color = Purple80,
+                    fontWeight = FontWeight(800),
+                )
+            ) {
+                append("Register ")
+            }
+
+        }
+
+        Text(
+            text = text,
+            style = TextStyle(
+                fontSize = 12.sp,
+                fontFamily = montserrat,
+                fontWeight = FontWeight(400),
+                color = Color(0xFFB6B6B6),
+
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun loginpreview(){
+    loginview()
+}
