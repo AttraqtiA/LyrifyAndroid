005757827ab8c58debe0523c51c36e13d15cd72c
GokaiInfinity <patriciofilbert@gmail.com>
2024-01-06 13:22:06 +0700

    Update Service

--- app/src/main/java/com/example/lyrifyapp/Service/MyDBService.kt
+++ app/src/main/java/com/example/lyrifyapp/Service/MyDBService.kt
@@ -19,34 +19,66 @@ interface MyDBService {
     @POST("login")
     suspend fun login(@Body user: User): APIResponse
 
-    @GET("user/{id}")
-    suspend fun getUser(@Path("user/{id}") id:Int):User
 
     @DELETE("logout")
     suspend fun logout():APIResponse
 
     //register
-    @POST("create_user")
-    suspend fun register(@Body user: User): APIResponse
-
-
-    @GET("histories")
-    suspend fun histories(): History
-    @GET("chapters")
-    suspend fun chapters(): Chapter
-    @GET("levels")
-    suspend fun levels(): Level
-    @GET("musics")
-    suspend fun musics(): Music
-
     @GET("all_user")
     suspend fun user(): User
+    @GET("user/{id}")
+    suspend fun getUser(@Path("user/{id}") id:Int):User
+    @POST("create_user")
+    suspend fun register(@Body user: User): APIResponse
 
     @PATCH("update_user/{id}")
     suspend fun update():APIResponse
 
     @DELETE("delete_user/{id}")
     suspend fun delete():APIResponse
+    
+//    @GET("user_music")
+//    suspend fun usermusic(): UserMusic
+//
+//    @POST("create_user_music")
+//    suspend fun createusermusic(@Body usermusic:UserMusic): APIResponse
+//
+//    @PATCH("update_user_music/{id}")
+//    suspend fun updateusermusic():APIResponse
+//
+//    @DELETE("delete_user_music/{id}")
+//    suspend fun deleteusermusic():APIResponse
+
+    //history
+    @GET("histories")
+    suspend fun histories(): History
+
+    @POST("create_history")
+    suspend fun register(@Body history: History): APIResponse
+    @PATCH("update_history/{id}")
+    suspend fun updatehistory():APIResponse
+
+    @DELETE("delete_history/{id}")
+    suspend fun deletehistory():APIResponse
+
+//chapter
+
+    @GET("chapters")
+    suspend fun chapters(): Chapter
+    //level
+
+    @GET("levels")
+    suspend fun levels(): Level
+
+    //music
+    @GET("musics")
+    suspend fun musics(): Music
+
+
+
+
+
+
     
 
 //    Route::post('create_user', [UserController::class, 'createUser']);
