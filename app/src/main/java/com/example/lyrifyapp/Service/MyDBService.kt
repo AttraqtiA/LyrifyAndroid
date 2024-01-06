9809c1e2d86b993a53ccb031e7a9a2dcf984f87b
GokaiInfinity <patriciofilbert@gmail.com>
2024-01-06 12:10:57 +0700

    MydbService

--- app/src/main/java/com/example/lyrifyapp/Service/MyDBService.kt
+++ app/src/main/java/com/example/lyrifyapp/Service/MyDBService.kt
@@ -1,10 +1,15 @@
 package com.example.lyrifyapp.Service
 
 import com.example.lyrifyapp.model.APIResponse
+import com.example.lyrifyapp.model.Chapter
+import com.example.lyrifyapp.model.History
+import com.example.lyrifyapp.model.Level
+import com.example.lyrifyapp.model.Music
 import com.example.lyrifyapp.model.User
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -24,6 +29,25 @@ interface MyDBService {
     @POST("create_user")
     suspend fun register(@Body user: User): APIResponse
 
+
+    @GET("histories")
+    suspend fun histories(): History
+    @GET("chapters")
+    suspend fun chapters(): Chapter
+    @GET("levels")
+    suspend fun levels(): Level
+    @GET("musics")
+    suspend fun musics(): Music
+
+    @GET("all_user")
+    suspend fun user(): User
+
+    @PATCH("update_user/{id}")
+    suspend fun update():APIResponse
+
+    @DELETE("delete_user/{id}")
+    suspend fun delete():APIResponse
+    
 
 //    Route::post('create_user', [UserController::class, 'createUser']);
 //    Route::post('login', [AuthenticationController::class, 'login'])->name('login');
